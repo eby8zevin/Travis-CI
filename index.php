5a8e41f318213fbe14f71bde8aa9b521e750178c
eby8zevin <36786100+eby8zevin@users.noreply.github.com>
2020-12-09 08:12:13 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>UAS Aplikasi Mobile 7</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+
+  <h1>Wes Dadi Mas :v</h1>
+  <p>Support by <a href="heroku.com">Heroku</a></p>
+
+</body>
+</html>
