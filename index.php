61795388df42ad08f0b969712deb7a6b6f736ba0
eby8zevin <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2021-06-17 11:05:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
 <?php
 
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__.'/vendor/autoload.php';
 
-$text = 'Hello, world. http://www.facebook.com';
-
-$linker = new \App\Utilities\Linker();
-$output = $linker->autolink($text);
-
-echo $output.PHP_EOL;
+?>
